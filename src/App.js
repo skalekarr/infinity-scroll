8b2463ed5c428d5c7133749a1fefe3cf95b79f0d
Sachin Kalekar <mr.sachinkalekar@gmail.com>
2019-05-28 19:39:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './components/list';
+import Detail from './components/detail';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const routing = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={List} />
+      <Route path="/details/:productId/:total" component={Detail} />
+    </Switch>
+  </BrowserRouter>
+)
 
-export default App;
+export default routing;
